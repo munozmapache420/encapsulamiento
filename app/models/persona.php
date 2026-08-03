0c82caca0b1316770087579826fb2e48705fed07
mapache <munozmapache420@gmail.com>
2026-08-03 15:19:33 -0500

    se actualiza el codigo

--- app/models/persona.php
+++ app/models/persona.php
@@ -18,24 +18,45 @@ class persona{
 }
 
 public function setNombre($nombre){
+    if (is_string($nombre) && trim($nombre) !== "")
+    {
     $this->nombre= $nombre;
+    }
 }
+
 public function setApellido($apellido){
-    $this->apellido= $apellido;
+    if (is_string($apellido) && trim($apellido) !== "")
+        {
+            $this->apellido= $apellido;
+        }
+    
 }
+
 public function setEdad($edad){
-    $this->edad= $edad;
+    if (is_numeric($edad) && $edad >= 18 && $edad <= 120 && trim($edad) !== "") {
+        $this->edad = $edad;
+    } else {
+        echo "Edad no válida.";
+    }
 }
+
 public function setCorreo($correo){
+        if (is_string($correo) && trim($correo) !== "")
+        {
     $this->correo= $correo;
+    }
 }
+
 public function setTrabajo($trabajo){
+        if (is_string($trabajo) && trim($trabajo) !== "")
+        {
     $this->trabajo= $trabajo;
+        }
 }
 
 
   public function saludar(){
-    echo "hola: " . $this->nombre . "</br>" ."tu apellido es: ". $this->apellido . "</br>" . " tu edad es: " . $this->edad . "</br>" ."tu correo es: " . $this->correo . "</br>". "trabajas en: " . $this->trabajo . "</br>";
+    echo "hola: " . $this->nombre . "</br>" ."tu apellido es: ". $this->apellido . "</br>" . " tu edad es: " . $this->edad . "</br>" ."tu correo es: " . $this->correo . "</br>". "trabajas en: " . $this->trabajo . "</br>". "</br>";
    }
 
 
@@ -60,6 +81,5 @@ public function setTrabajo($trabajo){
    }
 
 }
-
 
 ?>
--- public/index.php
+++ public/index.php
@@ -1,22 +1,22 @@
 <?php
 
-require_once "../clases/persona.php";
+require_once "../app/models/persona.php";
 
 //**$persona1= new persona("mapache","sadic",21,"munozmapache420@gmail.com","programador");
 //$persona2= new persona("marioLaCobrona","mario",35,"culoFijo24/7@gmail.com","trabajaDORA"); **\\
 
-$persona1= new persona("mapache","saidc",21,"correo", "programador");
-$persona2= new persona("marioCobradora", "cobradora", 35,"cobraodrsa390@gmial.com", "cobradora");
+$persona1= new persona("mapache","sadic",21,"munozmapache420@gmail", "programador");
+$persona2= new persona("mario", "cobradora", 35,"cobradora390@gmial.com", "cobradora");
 
 $persona1->saludar();
 $persona2->saludar();
 
 echo "</br>";
 
-echo "hol: " . $persona1->getNombre() . "</br>";
+echo "hola: " . $persona1->getNombre() . "</br>";
 echo "tienes una edad de: " . $persona1->getEdad() . "</br>";
 echo "</br>";
 
 echo "hola: ".   $persona2->getNombre() . "</br>";
-echo "tienes una edad de: " . $persona1->getEdad() . "</br>";
+echo "tienes una edad de: " . $persona2->getEdad(1) . "</br>";
 ?>
