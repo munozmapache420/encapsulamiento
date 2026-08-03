62feaa16b6a3ed57aa6974efc5db6cc7dd22d133
huber useche <kevinvelandia08123@gmail.com>
2026-08-03 12:36:47 -0500

    commit

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 require_once "../clases/persona.php";
 
 //**$persona1= new persona("mapache","sadic",21,"munozmapache420@gmail.com","programador");
-//$persona2= new persona("marioLaCobrona","chupadora",35,"culoFijo24/7@gmail.com","prostituta"); **\\
+//$persona2= new persona("marioLaCobrona","mario",35,"culoFijo24/7@gmail.com","trabajaDORA"); **\\
 
 $persona1= new persona("mapache","saidc",21,"correo", "programador");
 $persona2= new persona("marioCobradora", "cobradora", 35,"cobraodrsa390@gmial.com", "cobradora");
